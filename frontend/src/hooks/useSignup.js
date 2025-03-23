9ae83d631ec785bad5cbb60274e1f7e9c20a9aed
Mahnas P <mahnasmailbox@gmail.com>
2025-03-23 14:44:23 +0530

    refactor: update login and signup hooks to use axios instance for API calls

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,60 +1,63 @@
-import React, { useState } from 'react'
-import toast from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { login as authLogin } from "../store/authSlice";
 import axios from "axios";
 
 function validate({ username, email, password, confirmPassword }) {
-    if (!username || !email || !password || !confirmPassword) {
-        toast.error("Please fill in all fields");
-        return false;
-    }
-    if (password != confirmPassword) {
-        toast.error("passwords do not match!");
-        return false;
-    }
-    if (password.length < 6) {
-        toast.error("Password must be atleast 6 characters");
+  if (!username || !email || !password || !confirmPassword) {
+    toast.error("Please fill in all fields");
     return false;
-    }
-    return true;
+  }
+  if (password != confirmPassword) {
+    toast.error("passwords do not match!");
+    return false;
+  }
+  if (password.length < 6) {
+    toast.error("Password must be atleast 6 characters");
+    return false;
+  }
+  return true;
 }
 
 function useSignup() {
+  const authapi = axios.create({
+    baseURL: `${import.meta.env.VITE_API_URL}/api/auth`,
+  });
 
-    const [signuploading, setsignupLoading] = useState(false);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const [signuploading, setsignupLoading] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const signup = async ({ username, email, password, confirmPassword }) => {
-        const validated = validate({ username, email, password, confirmPassword });
-        if (!validated)
-            return;
-        
-        setsignupLoading(true);
-        try {
-            const response = await axios.post("/api/auth/signup", { username, email, password });
-            const data = response.data;
-            if (data.error)
-                throw new Error(response.data.error);
-            if (data) {
-                localStorage.setItem("userData", JSON.stringify(data.user));
-                localStorage.setItem("streamLoungeToken", JSON.stringify(data.token));
-                dispatch(authLogin(data.user));
-            }
-            document.getElementById("login_modal").close();
-            navigate("/");
-        } catch (error) {
-            toast.error(error.message);
-        }
-        finally {
-            setsignupLoading(false);
-        }
+  const signup = async ({ username, email, password, confirmPassword }) => {
+    const validated = validate({ username, email, password, confirmPassword });
+    if (!validated) return;
+
+    setsignupLoading(true);
+    try {
+      const response = await authapi.post("/signup", {
+        username,
+        email,
+        password,
+      });
+      const data = response.data;
+      if (data.error) throw new Error(response.data.error);
+      if (data) {
+        localStorage.setItem("userData", JSON.stringify(data.user));
+        localStorage.setItem("streamLoungeToken", JSON.stringify(data.token));
+        dispatch(authLogin(data.user));
+      }
+      document.getElementById("login_modal").close();
+      navigate("/");
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setsignupLoading(false);
     }
+  };
 
-
-    return {signuploading,signup};
+  return { signuploading, signup };
 }
 
-export default useSignup
+export default useSignup;
